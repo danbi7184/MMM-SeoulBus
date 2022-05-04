9705db6500d94740260ff1791ad7b636b909d865
danbi7184 <danbi7184@gmail.com>
2022-05-05 00:48:58 +0900

    Modifying js file & Add package.json

--- MMM-SeoulBus.js
+++ MMM-SeoulBus.js
@@ -6,28 +6,29 @@ Module.register("MMM-SeoulBus", {
 	  key1: "",
 	  key2: "",
 	  stSrch: "", // 정류소명
+		stId: "", // 정류소 ID
 	  header: "버스 도착 정보",
 	  updateInterval: 60000,
 	},
-  
+
 	getStyles: function () {
 	  return ["MMM-SeoulBus.css"];
 	},
-  
+
 	getHeader: function () {
 	  if (this.busInfo) {
 		return "<i class='fa fa-fw fa-bus'></i> " + this.config.header;
 	  }
-	  return "<i class='fa fa-fw fa-bus'></i> 지하철 정보";
+	  return "<i class='fa fa-fw fa-bus'></i> 버스 정보";
 	},
-  
+
 	start: function () {
 	  Log.info("Starting module: " + this.name);
 	  this.busInfo = [];
 	  var self = this;
 	  this.loaded = false;
 	},
-  
+
 	getDom: function () {
 	  var wrapper = document.createElement("div");
 	  if (!this.loaded) {
@@ -37,10 +38,42 @@ Module.register("MMM-SeoulBus", {
 	  busTable.className = "small";
 	  var bus = this.busInfo;
 
+		var RowArr = new Array();
+    var rtNmArr = new Array();
+    var arrmsg1Arr = new Array();
+    var arrmsg2Arr = new Array();
+		var reride_Num1Arr = new Array();
+		var reride_Num2Arr = new Array();
+
+		for(var i=0; i<bus.length; i++) {
+			if(!bus[i].arrmsg1._text.includes("운행종료") && !bus[i].arrmsg1._text.includes("출발대기")
+			&& !bus[i].arrmsg1._text.includes("회차대기")) {
+				RowArr[i] = 'row' + i;
+				rtNmArr[i] = 'rtNm' + i;
+				arrmsg1Arr[i] = 'arrmsg1' + i;
+
+				RowArr[i] = document.createElement("tr");
+				RowArr[i].className = "title bright";
+        busTable.appendChild(RowArr[i]);
+
+				rtNmArr[i] = document.createElement("td");
+				rtNmArr[i].innerHTML = bus[i].rtNm._text;
+        RowArr[i].appendChild(rtNmArr[i]);
+
+				arrmsg1Arr[i] = document.createElement("td");
+				arrmsg1Arr[i].innerHTML = bus[i].arrmsg1._text;
+        RowArr[i].appendChild(arrmsg1Arr[i]);
+
+				arrmsg1Arr[i] = document.createElement("td");
+				arrmsg1Arr[i].innerHTML = bus[i].arrmsg1._text;
+        RowArr[i].appendChild(arrmsg1Arr[i]);
+			}
+		}
+
 	  wrapper.appendChild(busTable);
 	  return wrapper;
 	},
-  
+
 	getBusInfo: function () {
 	  Log.info("Requesting bus info");
 	  this.sendSocketNotification("GET_BUS_DATA", {
@@ -48,7 +81,7 @@ Module.register("MMM-SeoulBus", {
 		identifier: this.identifier,
 	  });
 	},
-  
+
 	notificationReceived: function (notification, payload, sender) {
 	  switch (notification) {
 		case "DOM_OBJECTS_CREATED":
@@ -59,7 +92,7 @@ Module.register("MMM-SeoulBus", {
 		  break;
 	  }
 	},
-  
+
 	socketNotificationReceived: function (notification, payload) {
 	  switch (notification) {
 		case "BUS_DATA":
@@ -74,4 +107,3 @@ Module.register("MMM-SeoulBus", {
 	  }
 	},
   });
-  
--- node_helper.js
+++ node_helper.js
@@ -19,21 +19,21 @@ module.exports = NodeHelper.create({
   getData: async function (payload) {
     let self = this;
     var queryParams =
-	'?' + encodeURIComponent('serviceKey') + '=' + payload.config.key1 +
-	'&' + encodeURIComponent('stSrch') + '=' + encodeURIComponent(payload.config.stSrch);
-    var url = payload.config.sample1 + queryParams;
+	'?' + encodeURIComponent('serviceKey') + '=' + payload.config.key2 +
+	'&' + encodeURIComponent('stId') + '=' + encodeURIComponent(payload.config.stId);
+    var url = payload.config.sample2 + queryParams;
     request({
         url: url,
         method: "GET"
       }, function (error, response, body) {
         if (!error && response.statusCode === 200) {
           var result = convert.xml2json(body, { compact: true, spaces: 4 });
-          var data = JSON.parse(result).realtimeStationArrival;
-          if (data.hasOwnProperty("row")) {
-            var realtimeArrivalList = data.row;
-            self.sendSocketNotification("BUS_DATA", realtimeArrivalList);
+          var data = JSON.parse(result).response;
+          if(data.hasOwnProperty("msgBody") && Array.isArray(data.msgBody.itemList)) {
+              var busArrivalList = data.msgBody.itemList;
+              self.sendSocketNotification("BUS_DATA", busArrivalList);
           } else {
-            self.sendSocketNotification("BUS_DATA_ERROR", data);
+              self.sendSocketNotification("BUS_DATA_ERROR", data);
           }
         }
       });
