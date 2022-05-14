01cef5698275ef0e0525211c69295bddcc494005
danbi7184 <danbi7184@gmail.com>
2022-05-14 22:37:23 +0900

    Modifying js file

--- MMM-SeoulBus.js
+++ MMM-SeoulBus.js
@@ -39,6 +39,7 @@ Module.register("MMM-SeoulBus", {
 	  var bus = this.busInfo;
 
 		var RowArr = new Array();
+		var routeTypeArr = new Array();
     var rtNmArr = new Array();
     var arrmsg1Arr = new Array();
     var arrmsg2Arr = new Array();
@@ -49,6 +50,7 @@ Module.register("MMM-SeoulBus", {
 			if(!bus[i].arrmsg1._text.includes("운행종료") && !bus[i].arrmsg1._text.includes("출발대기")
 			&& !bus[i].arrmsg1._text.includes("회차대기")) {
 				RowArr[i] = 'row' + i;
+				routeTypeArr[i] = 'routeType' + i;
 				rtNmArr[i] = 'rtNm' + i;
 				arrmsg1Arr[i] = 'arrmsg1' + i;
 
@@ -56,23 +58,40 @@ Module.register("MMM-SeoulBus", {
 				RowArr[i].className = "title bright";
         busTable.appendChild(RowArr[i]);
 
+				routeTypeArr[i] = document.createElement("td");
 				rtNmArr[i] = document.createElement("td");
 				if(bus[i].routeType._text == "1") {
 					rtNmArr[i].className = "AirportBus"
+					routeTypeArr[i].className = "AirportBus"
+					routeTypeArr[i].innerHTML = "공항"
 				} else if(bus[i].routeType._text == "2") {
 					rtNmArr[i].className = "ShuttleBus"
+					routeTypeArr[i].className = "ShuttleBus"
+					routeTypeArr[i].innerHTML = "마을"
 				} else if(bus[i].routeType._text == "3") {
 					rtNmArr[i].className = "BlueBus"
+					routeTypeArr[i].className = "BlueBus"
+					routeTypeArr[i].innerHTML = "간선"
 				} else if(bus[i].routeType._text == "4") {
 					rtNmArr[i].className = "GreenBus"
+					routeTypeArr[i].className = "GreenBus"
+					routeTypeArr[i].innerHTML = "지선"
 				} else if(bus[i].routeType._text == "5") {
 					rtNmArr[i].className = "YellowBus"
+					routeTypeArr[i].className = "YellowBus"
+					routeTypeArr[i].innerHTML = "순환"
 				} else if(bus[i].routeType._text == "6") {
 					rtNmArr[i].className = "RedBus"
+					routeTypeArr[i].className = "RedBus"
+					routeTypeArr[i].innerHTML = "광역"
 				} else if(bus[i].routeType._text == "8") {
 					rtNmArr[i].className = "NormalBus"
+					routeTypeArr[i].className = "NormalBus"
+					routeTypeArr[i].innerHTML = "경기"
 				}
+
 				rtNmArr[i].innerHTML = bus[i].rtNm._text;
+				RowArr[i].appendChild(routeTypeArr[i]);
         RowArr[i].appendChild(rtNmArr[i]);
 
 				arrmsg1Arr[i] = document.createElement("td");
